db59a508ad7bbe61c8d6c1ff683421b3324b2a74
vasilen.sotirov <vasilen.sotirov@scalefocus.com>
2020-03-16 11:25:30 +0200

    [VS-FP-3]fixes on coding standards

--- public/components/router/http/Request.php
+++ public/components/router/http/Request.php
@@ -8,14 +8,17 @@ class Request
      * @var array
      */
     private $postParams;
+
     /**
      * @var array
      */
     private $getParams;
+
     /**
      * @var string
      */
     private $requestUri;
+
     /**
      * @var string
      */
@@ -26,16 +29,16 @@ class Request
      */
     private function __construct()
     {
-    $this->getParams = $_GET;
-    $this->postParams = $_POST;
-    $this->requestMethod = $_SERVER['REQUEST_METHOD'];
-    $this->requestUri = $_SERVER['REQUEST_URI'];
+        $this->getParams = $_GET;
+        $this->postParams = $_POST;
+        $this->requestMethod = $_SERVER['REQUEST_METHOD'];
+        $this->requestUri = $_SERVER['REQUEST_URI'];
     }
 
     /**
      * @return array
      */
-    public function postParams()
+    public function getPostParams()
     {
         return $this->postParams;
     }
@@ -43,7 +46,7 @@ class Request
     /**
      * @return array
      */
-    public function getParams()
+    public function getGetParams()
     {
         return $this->getParams;
     }
