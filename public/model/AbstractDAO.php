5296cf481ce1e6829e5da3ba3e5f4047941f4560
vasilen.sotirov <vasilen.sotirov@scalefocus.com>
2020-03-23 12:09:05 +0200

    [VS-FP-9]added types and removed fetch and fetchall methods

--- public/model/AbstractDAO.php
+++ public/model/AbstractDAO.php
@@ -2,10 +2,12 @@
 
 namespace model;
 
+use PDO;
+
 class AbstractDAO
 {
     /**
-     * @var \PDO
+     * @var $pdo
      */
     public $pdo;
     public $statement;
@@ -51,84 +53,64 @@ class AbstractDAO
     }
 
     /**
-     * @param $fetch
-     *
-     * @return array
+     * @param string $sql
+     * @param array $params
      */
-    public function fetch($fetch)
-    {
-        return $this->statement->fetch($fetch);
-    }
-
-    /**
-     * @param $fetch
-     *
-     * @return array
-     */
-    public function fetchAll($fetch)
-    {
-        return $this->statement->fetchAll($fetch);
-    }
-
-    /**
-     * @param $sql
-     * @param $params
-     */
-    public function prepareAndExecute($sql, $params)
+    public function prepareAndExecute($sql, $params = [])
     {
         $this->statement = $this->pdo->prepare($sql);
         $this->statement->execute([$params]);
     }
 
     /**
-     * @param $sql
-     * @param $params
+     * @param string $sql
+     * @param array $params
      *
      * @return array
      */
-    public function fetchAssoc($sql, $params)
+    public function fetchAssoc($sql, $params = [])
     {
         $this->prepareAndExecute($sql, $params);
 
-        return $this->fetch(\PDO::FETCH_ASSOC);
+        return $this->statement->fetch(\PDO::FETCH_ASSOC);
     }
 
     /**
-     * @param $sql
-     * @param $params
+     * @param string $sql
+     * @param array $params
      *
      * @return array
      */
-    public function fetchAllAssoc($sql, $params)
+    public function fetchAllAssoc($sql, $params = [])
     {
         $this->prepareAndExecute($sql, $params);
 
-        return $this->fetchAll(\PDO::FETCH_ASSOC);
+        return $this->statement->fetchAll(\PDO::FETCH_ASSOC);
     }
 
     /**
-     * @param $sql
-     * @param $params
+     * @param string $sql
+     * @param array $params
      */
-    public function insert($sql, $params)
-    {
-        $this->prepareAndExecute($sql, $params);
-    }
-
-    /**
-     * @param $sql
-     * @param $params
-     */
-    public function delete($sql, $params)
+    public function insert($sql, $params = [])
     {
         $this->prepareAndExecute($sql, $params);
     }
 
     /**
-     * @param $sql
-     * @param $params
+     * @param string $sql
+     * @param array $params
      */
-    public function update($sql, $params)
+    public function delete($sql, $params = [])
+    {
+        $this->prepareAndExecute($sql, $params);
+    }
+
+    /**
+     * @param string $sql
+     * @param array $params
+     */
+    public function update($sql, $params = [])
     {
         $this->prepareAndExecute($sql, $params);
     }
